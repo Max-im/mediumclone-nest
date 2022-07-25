5c53b1e2a9619eca9bb694aca3327e7002c77c90
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2022-07-25 22:55:28 +0300

    format tags response

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { TagService } from '@app/tag/tag.service';
-import { TagEntity } from '@app/tag/tag.entity';
 
 @Controller('tags')
 export class TagController {
     constructor(private readonly tagservice: TagService) {}
 
     @Get()
-    async getTags(): Promise<TagEntity[]> {
-        return await this.tagservice.getTags();
+    async getTags(): Promise<{tags: string[]}> {
+        const tags = await this.tagservice.getTags();
+        return {
+            tags: tags.map(tag => tag.name)
+        }
     }
 }
